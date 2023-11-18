be8457856527b05f9fc2dfa2cd28631567b7198a
Is720948 <is720948@iteso.mx>
2023-11-17 22:23:45 -0600

    Update google.js

--- CuntosApp/Backend/Routes/google.js
+++ CuntosApp/Backend/Routes/google.js
@@ -2,4 +2,10 @@ const router = require('express').Router()
 
 router.get('/', passport.authenticate('google'));
 
+app.get('/oauth2/redirect/google',
+  passport.authenticate('google', { failureRedirect: '/login', failureMessage: true }),
+  function(req, res) {
+    res.redirect('/');
+  })
+
 module.exports = router
