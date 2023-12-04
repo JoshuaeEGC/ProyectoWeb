f5e7a4202dcb2867ab3740652cab1aaf2ce5d1c9
Nicolas Teran Yuen <Alejandro740521@outlook.com>
2023-12-04 11:11:27 -0600

    deploy

---
+++ CuntosApp/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+
+const port = 4000
+
+const app = express();
+
+app.use("", express.static(path))
+
+app.get('*', function(req, res) {
+    const uri = path.join(__dirname, 'dist', 'index.html');
+    res.sendfile(uri);
+})
+
+app.listen(port, () => {
+    console.log('app is running');
+})
