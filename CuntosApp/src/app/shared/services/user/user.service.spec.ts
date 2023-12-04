11388a712c0c842bfd1e15319eef21ba4409f8bc
Bubulubu72 <Alejandro740521@outlook.com>
2023-12-03 21:31:17 -0600

    get google user

--- CuntosApp/src/app/shared/services/user/user.service.spec.ts
+++ CuntosApp/src/app/shared/services/user/user.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MongoService } from './mongo.service';
+import { UserService } from './user.service';
 
-describe('MongoService', () => {
-  let service: MongoService;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MongoService);
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
